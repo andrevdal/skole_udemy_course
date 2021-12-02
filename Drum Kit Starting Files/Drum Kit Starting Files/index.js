e76dbfa6ad115c8a3d62a60bd76572b87980a819
andrevdal <andreas.drevdal@gmail.com>
2021-12-02 19:15:27 +0100

    drumKitJS

--- Drum Kit Starting Files/Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/Drum Kit Starting Files/index.js
@@ -1,5 +1,50 @@
-document.querySelector(".set button").addEventListener("click", handleClick)
+let allButtons = document.querySelectorAll(".drum")
+let howManyButtons = document.querySelectorAll(".drum").length
+let crash = new Audio('sounds/crash.mp3');
+let snare = new Audio('sounds/snare.mp3');
+let kick = new Audio('sounds/kick-bass.mp3');
+let tom1 = new Audio('sounds/tom-1.mp3');
+let tom2 = new Audio('sounds/tom-2.mp3');
+let tom3 = new Audio("sounds/tom-3.mp3");
+let tom4 = new Audio("sounds/tom-4.mp3");
 
-function handleClick(){
-    console.log("click!");
+for (i = 0; i < howManyButtons; i++){
+    allButtons[i].addEventListener("click", (e) =>{
+        let drumKey = e.target.innerHTML
+
+
+        switch (drumKey) {
+            case "w":
+                crash.play()
+            break;
+        
+            case "a":
+                snare.play()
+            break;
+
+            case "s":
+                kick.play()
+            break;
+
+            case "d":
+                tom1.play()
+            break;
+
+            case "j":
+                tom2.play()
+            break;
+            
+            case "k":
+                tom3.play()
+            break;
+
+            case "l":
+                tom4.play()
+            break;
+
+            default:
+                console.log(drumKey);
+        }
+        
+    })
 }

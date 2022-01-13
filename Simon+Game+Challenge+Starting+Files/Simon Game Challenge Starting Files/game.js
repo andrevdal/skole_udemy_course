bb569630312bd6899d6d26493f6494f3ff6f2ae4
andrevdal <andreas.drevdal@gmail.com>
2022-01-13 22:26:24 +0100

    finished simon game

--- Simon+Game+Challenge+Starting+Files/Simon Game Challenge Starting Files/game.js
+++ Simon+Game+Challenge+Starting+Files/Simon Game Challenge Starting Files/game.js
@@ -1,14 +1,16 @@
 const colorSounds = {
-    blue: new Audio("sounds/blue.mp3"),
     red: new Audio("sounds/red.mp3"),
     green: new Audio("sounds/green.mp3"),
+    blue: new Audio("sounds/blue.mp3"),
     yellow: new Audio("sounds/yellow.mp3"),
   }  
-let clickedPattern = []
+let errorSound = new Audio("sounds/wrong.mp3")
 let buttonColors = ["red", "green", "blue", "yellow"]
+let clickedPattern = []
 let gamePattern = []
 let level = 0
 let game = false
+let nrClicked = 0
 
 $("body").keypress(()=>{
     if(game == false){
@@ -16,35 +18,46 @@ $("body").keypress(()=>{
         nextSequence();
     }})
 
-function checkAnswer (currentLevel, clickedColor){
-    console.log(currentLevel);
-    if(clickedColor == gamePattern[currentLevel]){
-        console.log("yes");
-        if(currentLevel == gamePattern.length){
-            console.log("yes");
+function checkAnswer (currentLevel){
+    if(gamePattern[currentLevel] === clickedPattern[currentLevel]){
+        if(clickedPattern.length === gamePattern.length){
+            setTimeout(()=>{
+                nextSequence();
+            }, 1000)
         }
     } else{
-        console.log("no");
+        console.log("noob");
+        $("body").addClass("game-over");
+        setTimeout(()=>{
+            $("body").removeClass("game-over")
+        }, 200);
+        $("h1").html("Game Over, Press Any Key to Restart")
+        errorSound.play();
+        level = 0
+        gamePattern = []
+        clickedPattern = []
         game = false
     }
 }
 function nextSequence (){
+    clickedPattern = []
+    nrClicked = 0
     let randomNumber = Math.floor(Math.random() * 4);
     let randomColor = buttonColors[randomNumber]
-    $("h1").html("Level " + level)
     level++;
+    $("h1").html("Level " + level)
     gamePattern.push(randomColor);
     animatePress(randomColor);
     playSound(Object.values(colorSounds)[randomNumber]);
 }
 $(".btn").click((e)=>{
     if(game == true){
+        nrClicked++;
         let chosenColor = e.currentTarget.id;
         clickedPattern.push(chosenColor)
         playSound(colorSounds[chosenColor]);
         animatePress(chosenColor);
-        checkAnswer(level - 1, chosenColor);
-        nextSequence();
+        checkAnswer(nrClicked - 1, chosenColor);
     }})
 function animatePress (currentColor){
     $("#" + currentColor).fadeOut("fast").fadeIn("fast").addClass("pressed");

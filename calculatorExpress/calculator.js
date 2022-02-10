184e85685f39c7ff787fdeef0134ff8d93293897
andrevdal <andreas.drevdal@gmail.com>
2022-02-10 17:51:51 +0100

    calculator and some other things i think

---
+++ calculatorExpress/calculator.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const app = express();
+
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.get("/",
+    (req, res)=>{
+    res.sendFile(__dirname + '/index.html' )
+})
+
+app.get("/BMI", 
+(req, res) => {
+    res.sendFile(__dirname + "/bmiCalculator.html")
+})
+
+app.post("/BMI", 
+(req, res) => {
+    let weight = Number(req.body.weight)
+    let height = Number(req.body.height)
+    let bmi = weight / Math.pow(height, 2)
+    console.log(weight, height, bmi, req.body);
+})
+
+app.post('/', 
+(req, res)=>{
+    let num1 = Number(req.body.num1)
+    let num2 = Number(req.body.num2)
+    let result = num1 + num2
+    res.send("The awnser is " + result)
+})
+
+app.listen(300)

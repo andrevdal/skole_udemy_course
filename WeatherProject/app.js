e7dbe9e15440dffab93786a961ba9a1aaa166e13
andrevdal <andreas.drevdal@gmail.com>
2022-02-15 23:18:42 +0100

    more node

--- WeatherProject/app.js
+++ WeatherProject/app.js
@@ -1,23 +1,40 @@
 const express = require("express");
 const https = require("https");
+const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", (req, res) => {
+  let city = req.body.cityName;
+  let appId = "7e3def5a2289962133f3bc12fc71e17c";
   const weatherUrl =
-    "https://api.openweathermap.org/data/2.5/weather?q=Asker&appid=7e3def5a2289962133f3bc12fc71e17c&units=metric";
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    city +
+    "&appid=" +
+    appId +
+    "&units=metric";
+
   https.get(weatherUrl, (httpsRes) => {
     console.log(httpsRes.statusCode);
     httpsRes.on("data", (data) => {
-      weatherData = JSON.parse(data)
-        let temprature = weatherData.main.temp
-        let weatherDescription = weatherData.weather[0].description
-        let weatherDescriptionIcon = weatherData.weather[0].icon
-        let weatherDescriptionIconImg = "http://openweathermap.org/img/wn/" + weatherDescriptionIcon + "@2x.png"
-        res.write("<h1>Vret I asker er " + weatherDescription + "</h1>")
-        res.write("<h1>Tempraturen i Asker er " + temprature + "<h1>")
-        res.write("<img src=" + weatherDescriptionIconImg + ">")
-        res.send()
+      weatherData = JSON.parse(data);
+      let temprature = weatherData.main.temp;
+      let weatherDescription = weatherData.weather[0].description;
+      let weatherDescriptionIcon = weatherData.weather[0].icon;
+      let weatherDescriptionIconImg =
+        "http://openweathermap.org/img/wn/" +
+        weatherDescriptionIcon +
+        "@2x.png";
+      res.write("<h1>Veret I " + city + " er " + weatherDescription + "</h1>");
+      res.write("<h1>Tempraturen i " + city + " er " + temprature + "<h1>");
+      res.write("<img src=" + weatherDescriptionIconImg + ">");
+      res.send();
     });
   });
 });

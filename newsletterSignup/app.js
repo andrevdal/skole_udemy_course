b70934cccc049fc4138e4f073c14c79705b2f339
andrevdal <andreas.drevdal@gmail.com>
2022-03-03 12:31:55 +0100

    set up nodeserver for mailchimp api

--- newsletterSignup/app.js
+++ newsletterSignup/app.js
@@ -4,11 +4,22 @@ const request = require("request")
 
 const app = express()
 
+app.use(express.static('.'))
+app.use(bodyParser.urlencoded({extended: true}))
+
 app.get("/", (req, res)=>{
     res.sendFile(__dirname + "/index.html")
-    console.log("It happend!!!")
+})
+
+app.post("/", (req, res)=>{
+
+    let name = req.body.name
+    let sirName = req.body.sirname
+    let email = req.body.email
+    console.log(name, sirName, email);
 })
 
 app.listen(600, ()=>{
     console.log("we gud in da hood on port 600");
 })
+// f16f92a9a4d8de17378d1ce74c0db0ff-us14
